68cc8f25cf21d3ad32ce8574fa21bfba98e934f6
quan200699 <maplestory2012vn@gmail.com>
2022-03-24 08:56:35 +0700

    thêm cấu hình jpa

--- src/main/java/com/codegym/config/AppConfig.java
+++ src/main/java/com/codegym/config/AppConfig.java
@@ -1,7 +1,5 @@
 package com.codegym.config;
 
-import com.codegym.dao.IProductDAO;
-import com.codegym.dao.ProductDAO;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -10,6 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -19,9 +24,15 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @EnableWebMvc
 @Configuration
 @ComponentScan("com.codegym")
+@EnableTransactionManagement//annoutation sử dụng để quản lý transaction
 @PropertySource("classpath:upload-file.properties")
 public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
     private ApplicationContext applicationContext;
@@ -76,8 +87,47 @@ public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
         return multipartResolver;
     }
 
-    @Bean
-    public IProductDAO productDAO(){
-        return new ProductDAO();
+    @Bean//Bean để khởi tạo entity manager
+    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
     }
+
+    @Bean//Bean để khởi tạo entity manager factory
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(datasource());
+        em.setPackagesToScan("com.codegym.model");
+        //VenderAdapter sử dụng để cấu hình liên quan đến hibernate
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+        return em;
+    }
+
+
+    @Bean //Data source để cấu hình đường dẫn đến DB
+    public DataSource datasource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/product_management_c11");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345678");
+        return dataSource;
+    }
+
+    @Bean //Bean để khai báo quản lý transaction => JPA tự quản lý
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    //Phương thức để khai báo các cấu hình liên quan hibernate
+    public Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        return properties;
+    }
+
 }
